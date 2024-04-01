391c74f1467db3d5b47ef088051c1b6c3421cc13
dewanakl <dewanakretarta29@gmail.com>
2024-04-01 07:52:24 +0700

    feat: fix admin count presence

--- app/Repositories/CommentRepositories.php
+++ app/Repositories/CommentRepositories.php
@@ -59,6 +59,7 @@ class CommentRepositories implements CommentContract
     {
         return Comment::where('user_id', $id)
             ->whereNull('parent_id')
+            ->where('is_admin', false)
             ->groupBy('user_id')
             ->select([
                 'SUM(CASE WHEN presence = TRUE THEN 1 ELSE 0 END) AS present_count',
