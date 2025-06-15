04df91e5b54d7479bb44cd2b0c9e8cfbec7ffb7e
dewanakl <dewanakretarta29@gmail.com>
2025-06-15 10:19:12 +0700

    refactor: improve timezone handling in TzMiddleware and enhance error response for empty timezone

--- app/Middleware/TzMiddleware.php
+++ app/Middleware/TzMiddleware.php
@@ -2,19 +2,25 @@
 
 namespace App\Middleware;
 
+use App\Response\JsonResponse;
 use Closure;
 use Core\Auth\Auth;
 use Core\Http\Request;
 use Core\Middleware\MiddlewareInterface;
 use Core\Support\Env;
 use Core\Support\Time;
+use DateTimeZone;
 
 final class TzMiddleware implements MiddlewareInterface
 {
     public function handle(Request $request, Closure $next)
     {
-        // override default timezone from env.
-        Env::set('TIMEZONE', Auth::user()->tz);
+        $tz = Auth::user()->getTimezone();
+        if (!in_array($tz, DateTimeZone::listIdentifiers(), true)) {
+            return (new JsonResponse)->errorBadRequest(['Invalid time zone']);
+        }
+
+        Env::set('TIMEZONE', $tz);
         Time::setTimezoneDefault();
 
         return $next($request);
